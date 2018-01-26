56567351e96ebd18205a67447d2e9d6054dc91f4
AlirieGray <caitlin.estrellita@gmail.com>
2018-01-26 12:57:45 -0800

    auth routes

---
+++ controllers/auth-controller.js
@@ -0,0 +1,28 @@
+
+module.exports = function(app) {
+  app.get('/login', function(req, res) {
+    console.log('GET login');
+    res.redirect('/');
+  });
+
+  app.post('/login', function(req, res, next) {
+    console.log('POST login');
+    res.redirect('/');
+  });
+
+  app.get('/logout', function(req, res) {
+    console.log('GET logout');
+    res.redirect('/');
+  });
+
+  app.post('/signup', function(req, res, next) {
+    console.log('POST signup');
+    res.redirect('/');
+  });
+
+  app.changeEvent('/signup', function(req, res, next) {
+    console.log('GET signup');
+    res.redirect('/');
+  });
+}
+}
