33cacd2b97ad383423c7f7975e4cc8392797ae62
Tassos <lambrou@gmail.com>
2018-01-30 00:34:39 -0800

    Added starter to server.js and initialized sequelize

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 
 //**** DEPENDENCIES ****//
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -7,10 +8,32 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+//**** SEQUELIZE ****//
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('rubric_dev', 'postgres', null, { dialect: 'postgres' });
+const sync = () => {
+  return sequelize.sync({ force: true })
+}
+
+//**** ALLOW CORS ****//
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
 //**** MIDDLEWARE ****//
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(acceptOverride())
+app.use(function (req, res, next) {
+  var format = req.query.format
+  if (format) { req.headers.accept = 'application/' + format }
+  next();
+});
 
 // **** CONTROLLERS **** //
 require('./controllers/auth-controller.js')(app);
@@ -21,8 +44,11 @@ app.get('/', function(req, res) {
   res.send('index');
 });
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8000;
 
 app.listen(PORT, function(req, res) {
-  console.log("listening!");
+  console.log("Rubrics App listening on port 8000!");
+  sync()
+    .then(() => console.log('... and Database synced!'))
+    .catch( e => console.log(e))
 });
