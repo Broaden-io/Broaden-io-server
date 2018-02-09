7eb423bf759772b31cd865a0f4d7a16c788fe682
Tassos <lambrou@gmail.com>
2018-02-08 16:54:48 -0800

    Finished CRUD of Rubrics.  Show route still needs to be updated as it will become the index routes for all the child nested resources

--- controllers/rubrics-controller.js
+++ controllers/rubrics-controller.js
@@ -1,14 +1,79 @@
 
-module.exports = function(app) {
-  app.get('/rubrics', function(req, res) {
-    res.send('sending all rubrics');
+module.exports = (app) => {
+
+  // Index of all Rubrics
+  app.get('/rubrics', (req, res) => {
+    db.Rubric.findAll()
+    .then((rubrics) => {
+      console.log("Response from Rubric/Index: ", rubrics)
+      res.json(rubrics)
+    })
+    .catch((err) => {
+      if (err) {
+        console.log("Uhh oh!! ", err.message)
+        res.error(err)
+      }
+    })
   });
 
-  app.get('/rubrics/:id', function(req, res) {
-    res.send('getting rubric by id');
+  // TODO: Still need to implement a nested retrieval of all child resources
+  // Show/Edit a Rubric
+  app.get('/rubrics/:id', (req, res) => {
+    const rubricId = req.params.id
+    db.Rubric.findById(rubricId)
+    .then((rubric) => {
+      console.log("Response from Rubric/Show: ", rubric)
+      res.status(200)
+      res.json({
+        message: "Rubric request successful",
+        rubric
+      })
+    })
+    .catch((err) => {
+      if (err) {
+        console.log("Uhh oh!! ", err.message)
+        res.error(err)
+      }
+    })
   });
 
-  app.post('/rubrics/new', function(req, res) {
-    res.send('creating new rubric');
+  // Create a Rubric
+  app.post('/rubrics/create', (req, res) => {
+    db.Rubric.create(req.body)
+    .then((rubric) => {
+      console.log("Response from Rubric/Create: ", rubric)
+      res.status(200)
+      res.json({
+        msg: 'Rubric added successfully!',
+        rubric
+      })
+    })
+    .catch((err) => {
+      if (err) {
+        console.log("Uhh oh!! ", err.message)
+        res.status(401)
+        res.error(err)
+      }
+    })
+  })
+
+  // Delete a Rubric
+  app.delete('/rubrics/:id/delete', (req, res) => {
+    const rubricId = req.params.id
+    db.Rubric.destroy({ where: { id: rubricId } })
+    .then((response) => {
+      console.log("Response from Rubric/Delete: ", response)
+      res.status(200)
+      res.json({
+        msg: 'Rubric deleted successfully!',
+        qty: response
+      })
+    })
+    .catch((err) => {
+      if (err) {
+        console.log("Uhh oh!! ", err.message)
+        res.error(err)
+      }
+    })
   })
 }
