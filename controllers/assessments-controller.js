7b0becde41987ead2a7c6411adba42bd407378b5
Jeff Chiu <jeffchiu2000@gmail.com>
2018-03-28 14:58:14 -0700

    clean up the assessment routes

--- controllers/assessments-controller.js
+++ controllers/assessments-controller.js
@@ -15,17 +15,10 @@ module.exports = (app) => {
         include: [{
           model: db.Scale,
           include: [{
-            model: db.Criterion,
-            include: [{
-              model: db.Action,
-            }],
-            order: [ [ { model: db.Action }, 'id', 'ASC' ] ]
+            model: db.Criterion
           }],
-          order: [ [ { model: db.Criterion }, 'id', 'ASC' ] ]
         }],
-        order: [ [ { model: db.Scale }, 'id', 'ASC' ] ]
       }],
-      order: [ [ { model: db.Competency }, 'id', 'ASC' ] ]
     })
     .then((rubric) => {
       console.log("Rubric: ", rubric.name)
@@ -34,6 +27,8 @@ module.exports = (app) => {
         rubricId: req.params.rubricId,
         rubricJSON: rubric,
       }
+      //https://github.com/sequelize/sequelize/issues/6554
+      //Sequelize findOrCreate and invoking spread()
       return db.Assessment.findOrCreate({ where: params, defaults: assessment })
     })
     .spread((assessment, created) => {
@@ -53,28 +48,6 @@ module.exports = (app) => {
         error: err
       })
     })
-
-    // db.Assessment.findOrCreate({ where: params, defaults: req.body })
-    // .all().then((assessment, created) => {
-    //   callback && callback(null, assessment, created);
-    // },
-    // (error) => {
-    //   callback && callback(error);
-    // });
-    //
-    // const callback = (error, result, created) => {
-    //   if (error) {
-    //     console.log("Error in Assessment Create Route:", error);
-    //     res.send(400, { message: "Error!", error })
-    //   } else {
-    //     console.log("New Assessment Created? ", created)
-    //     res.send({
-    //       message: (created ? "Assessment was successfully created" : "Assessment was successfully found"),
-    //       created,
-    //       result
-    //     })
-    //   }
-    // };
   });
 
 
