3ef88ad3477fbdd80c7c54d238c1b5d439f6daa8
Tassos Lambrou <lambrou@gmail.com>
2018-04-07 02:49:16 -0700

    Corrected Full Stack Mobile description

--- seeders/20180219065639-RubricsSeed.js
+++ seeders/20180219065639-RubricsSeed.js
@@ -5,7 +5,7 @@ module.exports = {
 
     return queryInterface.bulkInsert('Rubrics', [
               { name: "Full Stack Web Skills", iconName: 'web', description: "Make School's rubric for assessing competencies and getting a job as a professional in full stack web", userId: 10005, createdAt: new Date(), updatedAt: new Date(), id: 10000 },
-              { name: "Full Stack Mobile Skills", iconName: 'stay_current_portrait', description: "Make School's rubric for assessing course performance in any course", userId: 10005, createdAt: new Date(), updatedAt: new Date(), id: 10001 },
+              { name: "Full Stack Mobile Skills", iconName: 'stay_current_portrait', description: "Make School's rubric for assessing competencies and getting a job as a professional in full stack mobile", userId: 10005, createdAt: new Date(), updatedAt: new Date(), id: 10001 },
               { name: "Success Skills", iconName: 'wb_sunny', description: "Make School's rubric for assessing competencies for professional skills and best practices", userId: 10005, createdAt: new Date(), updatedAt: new Date(), id: 10002 },
               { name: "Internship & Job Readiness", iconName: 'work', description: "Make School's rubric for assessing competencies and readiness for getting a job/internship", userId: 10005, createdAt: new Date(), updatedAt: new Date(), id: 10003 },
               { name: "Technical Interview: Junior Lvl Engineer", iconName: 'code', description: "Make School's rubric for assessing competencies technical interviewing and whiteboarding", userId: 10005, createdAt: new Date(), updatedAt: new Date(), id: 10004 },
