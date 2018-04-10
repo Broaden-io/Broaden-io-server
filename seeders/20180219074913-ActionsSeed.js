f614c6fe492eb655c9d2489710c3623e651ca060
Jeff Chiu <jeffchiu2000@gmail.com>
2018-04-09 19:04:03 -0700

    remove duplication inside the name column

--- seeders/20180219074913-ActionsSeed.js
+++ seeders/20180219074913-ActionsSeed.js
@@ -6,7 +6,7 @@ module.exports = {
     return queryInterface.bulkInsert('Actions', [
 
       { name: "HTML Boilerplate", url: "https://github.com/h5bp/html5-boilerplate/blob/master/src/index.html" , criterionId: 10000, createdAt: new Date(), updatedAt: new Date(), id: 10000 },
-      { name: "Basic HTML", url: "https://www.w3schools.com/html/" , criterionId: 10001, createdAt: new Date(), updatedAt: new Date(), id: 10001 },
+      { name: "Basic HTML", url: "https://www.w3schools.com/html/html_basic.asp" , criterionId: 10001, createdAt: new Date(), updatedAt: new Date(), id: 10001 },
       { name: "Basic HTML", url: "https://www.w3schools.com/html/html_basic.asp" , criterionId: 10001, createdAt: new Date(), updatedAt: new Date(), id: 10002 },
       { name: "HTML", url: "https://www.youtube.com/watch?v=UB1O30fR-EE" , criterionId: 10001, createdAt: new Date(), updatedAt: new Date(), id: 10003 },
       { name: "HTML Tags Memorization", url: "https://www.youtube.com/watch?v=QZXDAGwD8Ng" , criterionId: 10002, createdAt: new Date(), updatedAt: new Date(), id: 10004 },
@@ -28,12 +28,12 @@ module.exports = {
       { name: "CSS attributes", url: "http://www.htmldog.com/references/css/properties/" , criterionId: 100014, createdAt: new Date(), updatedAt: new Date(), id: 100020 },
       { name: "CSS Bootstrap", url: "https://getbootstrap.com/docs/3.3/css/" , criterionId: 100015, createdAt: new Date(), updatedAt: new Date(), id: 100021 },
       { name: "Bootstrap Convention", url: "https://www.ostraining.com/blog/coding/bootstrap-right-way/" , criterionId: 100016, createdAt: new Date(), updatedAt: new Date(), id: 100022 },
-      { name: "Modify frameworks at will", url: "https://www.webdesignerdepot.com/2013/07/how-to-modify-bootstrap-simply-and-effectively/" , criterionId: 100017, createdAt: new Date(), updatedAt: new Date(), id: 100023 },
+      { name: "", url: "https://www.webdesignerdepot.com/2013/07/how-to-modify-bootstrap-simply-and-effectively/" , criterionId: 100017, createdAt: new Date(), updatedAt: new Date(), id: 100023 },
       { name: "", url: "https://www.quora.com/How-do-you-make-a-web-page-responsive-without-Bootstrap-using-only-HTML-and-CSS" , criterionId: 100018, createdAt: new Date(), updatedAt: new Date(), id: 100024 },
       { name: "Float & FlexBox", url: "https://www.w3schools.com/css/css3_flexbox.asp" , criterionId: 100019, createdAt: new Date(), updatedAt: new Date(), id: 100025 },
       { name: "CSS with Mobile Responsive", url: "https://www.w3schools.com/css/css_rwd_mediaqueries.asp" , criterionId: 100020, createdAt: new Date(), updatedAt: new Date(), id: 100026 },
       { name: "advanced CSS3", url: "http://www.htmldog.com/guides/css/advanced/" , criterionId: 100021, createdAt: new Date(), updatedAt: new Date(), id: 100027 },
-      { name: "Advanced CSS", url: "http://www.htmldog.com/guides/css/advanced/transitions/" , criterionId: 100022, createdAt: new Date(), updatedAt: new Date(), id: 100028 },
+      { name: "", url: "http://www.htmldog.com/guides/css/advanced/transitions/" , criterionId: 100022, createdAt: new Date(), updatedAt: new Date(), id: 100028 },
       { name: "Advanced CSS", url: "https://css-tricks.com/" , criterionId: 100022, createdAt: new Date(), updatedAt: new Date(), id: 100029 },
       { name: "Advanced CSS", url: "https://css-tricks.com/" , criterionId: 100022, createdAt: new Date(), updatedAt: new Date(), id: 100030 },
       { name: "Advanced CSS", url: "https://css-tricks.com/" , criterionId: 100023, createdAt: new Date(), updatedAt: new Date(), id: 100031 },
@@ -44,18 +44,7 @@ module.exports = {
       { name: "JavaScript", url: "https://www.w3schools.com/html/html_scripts.asp" , criterionId: 10024, createdAt: new Date(), updatedAt: new Date(), id: 10036 },
       { name: "JavaScript", url: "https://www.codecademy.com/catalog/language/javascript" , criterionId: 10024, createdAt: new Date(), updatedAt: new Date(), id: 10037 },
       { name: "JavaScript", url: "https://www.codecademy.com/catalog/language/javascript" , criterionId: 10024, createdAt: new Date(), updatedAt: new Date(), id: 10038 },
-      { name: "JavaScript", url: "https://www.codecademy.com/catalog/language/javascript" , criterionId: 10025, createdAt: new Date(), updatedAt: new Date(), id: 10039 },
-      { name: "JavaScript", url: "https://www.codecademy.com/catalog/language/javascript" , criterionId: 10026, createdAt: new Date(), updatedAt: new Date(), id: 10040 },
-      { name: "JavaScript", url: "https://www.codecademy.com/catalog/language/javascript" , criterionId: 10027, createdAt: new Date(), updatedAt: new Date(), id: 10041 },
-      { name: "JavaScript", url: "https://www.codecademy.com/catalog/language/javascript" , criterionId: 10028, createdAt: new Date(), updatedAt: new Date(), id: 10042 },
-      { name: "JavaScript", url: "https://www.codecademy.com/catalog/language/javascript" , criterionId: 10029, createdAt: new Date(), updatedAt: new Date(), id: 10043 },
-      { name: "JavaScript", url: "https://www.codecademy.com/catalog/language/javascript" , criterionId: 10030, createdAt: new Date(), updatedAt: new Date(), id: 10044 },
-      { name: "JavaScript", url: "https://www.codecademy.com/catalog/language/javascript" , criterionId: 10031, createdAt: new Date(), updatedAt: new Date(), id: 10045 },
-      { name: "JavaScript", url: "https://www.codecademy.com/catalog/language/javascript" , criterionId: 10032, createdAt: new Date(), updatedAt: new Date(), id: 10046 },
-      { name: "JavaScript", url: "https://www.codecademy.com/catalog/language/javascript" , criterionId: 10033, createdAt: new Date(), updatedAt: new Date(), id: 10047 },
-      { name: "JavaScript", url: "https://www.codecademy.com/catalog/language/javascript" , criterionId: 10034, createdAt: new Date(), updatedAt: new Date(), id: 10048 },
-      { name: "JavaScript", url: "https://www.codecademy.com/catalog/language/javascript" , criterionId: 10035, createdAt: new Date(), updatedAt: new Date(), id: 10049 },
-      { name: "JavaScript", url: "https://www.codecademy.com/catalog/language/javascript" , criterionId: 10036, createdAt: new Date(), updatedAt: new Date(), id: 10050 },
+
       // { name: "Writing client side", url: , criterionId: 1000, createdAt: new Date(), updatedAt: new Date(), id: 1000 },
     ], {});
     /*
