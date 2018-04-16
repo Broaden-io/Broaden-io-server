06159eb0064fbadbfc4e2ad8f6d6fb94148a772b
Shane Austrie <yoyo.rom.dev@gmail.com>
2018-04-16 07:18:52 -0700

    Corrected ids on scales and criteria

--- seeders/20180219074747-CompetenciesSeed.js
+++ seeders/20180219074747-CompetenciesSeed.js
@@ -20,11 +20,11 @@ module.exports = {
               // { name: "Testing and Debugging",  rubricId: 10001, createdAt: new Date(), updatedAt: new Date(), id: 10012 },
               // { name: "Databases - SQL and NoSQL",  rubricId: 10001, createdAt: new Date(), updatedAt: new Date(), id: 10013 },
 
-              { name: "Professionalism",  rubricId: 10002, createdAt: new Date(), updatedAt: new Date(), id: 10014 },
-              { name: "Productivity",  rubricId: 10002, createdAt: new Date(), updatedAt: new Date(), id: 10015 },
-              { name: "Teamwork and Feedback",  rubricId: 10002, createdAt: new Date(), updatedAt: new Date(), id: 10016 },
-              { name: "Emotional Intelligence",  rubricId: 10002, createdAt: new Date(), updatedAt: new Date(), id: 10017 },
-              { name: "Communication",  rubricId: 10002, createdAt: new Date(), updatedAt: new Date(), id: 10018 },
+              { name: "Professionalism",  rubricId: 10002, createdAt: new Date(), updatedAt: new Date(), id: 20014 },
+              { name: "Productivity",  rubricId: 10002, createdAt: new Date(), updatedAt: new Date(), id: 20015 },
+              { name: "Teamwork and Feedback",  rubricId: 10002, createdAt: new Date(), updatedAt: new Date(), id: 20016 },
+              { name: "Emotional Intelligence",  rubricId: 10002, createdAt: new Date(), updatedAt: new Date(), id: 20017 },
+              { name: "Communication",  rubricId: 10002, createdAt: new Date(), updatedAt: new Date(), id: 20018 },
 
           ], {});
     /*
