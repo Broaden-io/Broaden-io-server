11b087b3d75e6f6996f2a13473a0631b9ae23d8e
Tassos Lambrou <lambrou@gmail.com>
2018-04-15 17:59:04 -0700

    added creator and meta columns to the Action model

--- models/action.js
+++ models/action.js
@@ -2,7 +2,16 @@
 module.exports = (sequelize, DataTypes) => {
   var Action = sequelize.define('Action', {
     name: DataTypes.STRING,
-    url: DataTypes.STRING
+    url: DataTypes.STRING,
+    meta: DataTypes.JSON,
+    creator: {
+      type: DataTypes.INTEGER,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    }
   }, {
     classMethods: {
       associate: function(models) {
