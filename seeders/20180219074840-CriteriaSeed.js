77ae74fcdcaa94b1a37576a30c46f8046e1e41c9
Tassos Lambrou <lambrou@gmail.com>
2018-04-15 17:09:40 -0700

    Fixed some typos conflicting with validation.  Still not a working see yet for criteria

--- seeders/20180219074840-CriteriaSeed.js
+++ seeders/20180219074840-CriteriaSeed.js
@@ -219,7 +219,7 @@ module.exports = {
 
               { text: "Rushes to answer questions, is fidgety and distracted, looks away from recruiters",  level: 1, scaleId: 10089, createdAt: new Date(), updatedAt: new Date(), id: 10178 },
               { text: "Speaks softly, sometimes without clear direction in thought",  level: 2, scaleId: 10089, createdAt: new Date(), updatedAt: new Date(), id: 10179 },
-              { text: "Speaks confidently with minor fidgets and 'ums'",  level: 3, scaleId: 100879, createdAt: new Date(), updatedAt: new Date(), id: 10180 },
+              { text: "Speaks confidently with minor fidgets and 'ums'",  level: 3, scaleId: 10879, createdAt: new Date(), updatedAt: new Date(), id: 10180 },
               { text: "Speaks confidently with upright posture, without any 'ums' or 'uhs'",  level: 4, scaleId: 10089, createdAt: new Date(), updatedAt: new Date(), id: 10181 },
 
               { text: "Has difficulty explaining who they are and their career goals",  level: 1, scaleId: 10090, createdAt: new Date(), updatedAt: new Date(), id: 10182 },
@@ -244,10 +244,10 @@ module.exports = {
 
               { text: "Did not use pseudocode.",  level: 1, scaleId: 10094, createdAt: new Date(), updatedAt: new Date(), id: 10198 },
               { text: "Used pseudocode",  level: 2, scaleId: 10094, createdAt: new Date(), updatedAt: new Date(), id: 10199 },
-              { text: "Used pseudocode to chat through various implementations of a solution with the interviewer, identify edge cases, while integrating the interviewer’s suggestions and hints.",  level: 3, scaleId: 10094, createdAt: new Date(), updatedAt: new Date(), id: 10100 },
+              { text: "Used pseudocode to chat through various implementations of a solution with the interviewer, identify edge cases, while integrating the interviewer’s suggestions and hints.",  level: 3, scaleId: 10094, createdAt: new Date(), updatedAt: new Date(), id: 10200 },
 
-              { text: "Attempted to solve a complex case before decomposing it into simpler elements.",  level: 1, scaleId: 10095, createdAt: new Date(), updatedAt: new Date(), id: 10102 },
-              { text: "Decomposed the question into simpler parts and then synthesized a more complex solution",  level: 2, scaleId: 10095, createdAt: new Date(), updatedAt: new Date(), id: 10103 },
+              { text: "Attempted to solve a complex case before decomposing it into simpler elements.",  level: 1, scaleId: 10095, createdAt: new Date(), updatedAt: new Date(), id: 10202 },
+              { text: "Decomposed the question into simpler parts and then synthesized a more complex solution",  level: 2, scaleId: 10095, createdAt: new Date(), updatedAt: new Date(), id: 10203 },
 
           ], {});
 
