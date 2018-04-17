38c110680986acbc0a45e1f7e6a21fe9e9c0c1be
Jeff Chiu <jeffchiu2000@gmail.com>
2018-04-16 19:43:23 -0700

    Profile route update (#41)
    * added update the user controller and remove the profile route
    
    * fixed the error on the user controller for edit users
    
    * update the user controllers
    
    * update the backend routes for get user profile
    
    * update POSTMAN API

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,3 +1,12 @@
+// const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcrypt')
+// const db = require('../models')
+// const multer  = require('multer')
+// const Upload = require('s3-uploader');
+// const uploadService = multer({storage: multer.memoryStorage(), limits: {fileSize: 1000 * 1000 * 12}});
+// const AWS = requrie('aws-sdk')
+
+
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt')
 const db = require('../models')
@@ -33,50 +42,57 @@ module.exports = function(app) {
         });
       }
     })
-
   });
 
-  //SHOW PROFILE
-  app.get('/profile', function(req, res) {
-    if (req.user) {
-      const username = req.user.username;
-      db.User.findOne({ where: { username } }).then((userData) => {
-        const user = userData.dataValues;
-        if (!user) {
-          // User not found
-          return res.status(400).send({ message: 'Cannot get user' });
-        }
-        res.status(200);
-        res.json({
-          username: user.username,
-          email: user.email,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          bio: user.bio,
-          avatarURL: user.avatarURL
-        });
-      })
-      .catch((err) => {
-        if (err) {
+
+    // UPDATE USER
+    app.put('/users/:username', function(req, res) {
+      console.log('PUT user:', req.params.userId);
+      const user = req.body
+      const username = req.params.username
+
+      db.User.update(user, {
+        where: { username: username }
+      }).then((response) => {
+          res.status(200)
+          res.json({
+            message: 'username updated successfully!',
+          })
+        }).catch((err) => {
+          console.log(err);
           res.status(400);
           res.json({
-            message: "Cannot get user"
-          });
-        }
-      })
-    } else {
-      res.status(401);
-      res.json({
-        message: "Not logged in"
-      })
-    }
-  })
+            message: "Error!",
+            error: err
+          })
+        })
+      });
 
-  //UPDATE USER
-  app.put('/users/:userId', function(req, res) {
-    console.log('PUT user:', req.params.userId);
-    res.redirect('/');
-  });
+
+    // Get User information
+    app.get('/users/:username', function(req, res) {
+      // console.log('PUT user:', req.params.userId);
+      const username = req.params.username
+
+      const userId = req.params.id
+      const user = req.body
+      db.User.findOne(username, {
+        where: { username: username }
+      }).then((response) => {
+          res.status(200)
+          res.json({
+            message: 'Get user successfully!',
+          })
+        }).catch((err) => {
+          console.log(err);
+          res.status(400);
+          res.json({
+            message: "Error!",
+            error: err
+          })
+        })
+      });
+
 
   //DELETE USER
   app.delete('/users/:userId', function(req, res) {
