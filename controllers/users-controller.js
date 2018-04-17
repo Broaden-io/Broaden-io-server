634625e258a4086e878150bb8bd4a9b4d0f7cff5
Alirie Gray <alirie.gray@gmail.com>
2018-04-16 19:41:33 -0700

    Revert "Adding profiles information" (#40)

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,48 +1,8 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt')
 const db = require('../models')
-const multer  = require('multer')
-const Upload = require('s3-uploader');
-const uploadService = multer({storage: multer.memoryStorage(), limits: {fileSize: 1000 * 1000 * 12}});
-const AWS = requrie('aws-sdk')
 
-
-//
-// module.exports = function(app) {
-//
-// //Use Multer uploader
-//
-// // Amazon s3 config
-// const s3 = new AWS.S3()
-// AWS.config.update(
-//   {
-//     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-//     subregion: 'us-west-2',
-//   });
-//
-// const router = new express.Router();
-//
-// // Multer config
-// // memory storage keeps file data in a buffer
-// const upload = multer({
-//   storage: multer.memoryStorage(),
-//   // file size limitation in bytes
-//   limits: { fileSize: 52428800 },
-// });
-
-// router.post('/upload', upload.single('theseNamesMustMatch'), (req, res) => {
-//   // req.file is the 'theseNamesMustMatch' file
-//   s3.putObject({
-//       Bucket: 'your-bucket-name',
-//       Key: 'your-key-name',
-//       Body: req.file.buffer,
-//       ACL: 'public-read', // your permisions
-//     }, (err) => {
-//       if (err) return res.status(400).send(err);
-//       res.send('File uploaded to S3');
-// })
-
+module.exports = function(app) {
 
   //SHOW USER
   app.get('/users/:userId', function(req, res) {
@@ -112,37 +72,11 @@ const AWS = requrie('aws-sdk')
     }
   })
 
-  // UPDATE USER
+  //UPDATE USER
   app.put('/users/:userId', function(req, res) {
     console.log('PUT user:', req.params.userId);
-
     res.redirect('/');
   });
-
-  // Get User information
-  app.get('/user/:username', function(req, res) {
-    // console.log('PUT user:', req.params.userId);
-    const username = req.params.username
-
-    const userId = req.params.id
-    const user = req.body
-    db.User.update(user, {
-      where: { username: username }
-    }).then((response) => {
-        res.status(200)
-        res.json({
-          message: 'Get user successfully!',
-        })
-      }).catch((err) => {
-        console.log(err);
-        res.status(400);
-        res.json({
-          message: "Error!",
-          error: err
-        })
-      })
-    });
-
 
   //DELETE USER
   app.delete('/users/:userId', function(req, res) {
